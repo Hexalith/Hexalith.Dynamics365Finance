1723c6c7b3f3b7b54caed7e0b6d2dd00b365ae59
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-05-10 18:45:00 +0200

    update build ci

--- src/Servers/publish_containers.ps1
+++ src/Servers/publish_containers.ps1
@@ -1,6 +1,7 @@
 az login --tenant christofle.com
 az acr login -n christofle
 $version=(minver -t v -v e -d preview)
+dotnet restore --no-cache
 cd ./Hexalith.Server.Dynamics365Finance
 dotnet publish /t:PublishContainer -c Release -p ContainerRegistry="christofle.azurecr.io" -p ContainerImageTag=$version
 pause
