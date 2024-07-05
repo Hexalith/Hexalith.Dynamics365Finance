d89917329deb553bc2bcc95644510371c8334bfc
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-07-05 15:32:04 +0200

    fix publish container

--- src/Servers/publish_containers.ps1
+++ src/Servers/publish_containers.ps1
@@ -3,7 +3,7 @@ az acr login -n christofle
 $version=(minver -t v -v e -d preview)
 dotnet restore --no-cache
 cd ./Hexalith.Server.Dynamics365Finance
-dotnet restore --no-cache
+dotnet restore --no-cache --force --disable-build-servers --force-evaluate  
 dotnet build --no-restore --disable-build-servers --no-incremental -c Release 
 dotnet publish --no-build /t:PublishContainer -c Release -p ContainerRegistry="christofle.azurecr.io" -p ContainerImageTag=$version
 pause
